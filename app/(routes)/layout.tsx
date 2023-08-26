f8a9c0469fb31aeb54f53261f62422355653b739
Takuyawe <takuslugger200@gmail.com>
2023-08-26 14:52:04 +0900

    added id app to layout.tsx

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body id="app" className={inter.className}>
         <ContextProvider>{children}</ContextProvider>
       </body>
     </html>
